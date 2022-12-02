247d4e2dee1ade5dfa5ef8b4dbdd7a8c936a6f44
John McDonough <movinalot@gmail.com>
2022-12-02 00:58:06 -0500

    initial commit

---
+++ 2022/aoc_d02p1.py
@@ -0,0 +1,77 @@
+"""
+John McDonough
+    github - movinalot
+    Advent of Code 2022
+"""
+
+TESTING = 0
+DEBUG = 0
+DAY = "02"
+YEAR = "2022"
+PART = "1"
+ANSWER = None
+
+if TESTING:
+    puzzle_data = [ 
+        "A Y",
+        "B X",
+        "C Z"
+    ]
+else:
+    with open("puzzle_data_" + DAY + "_" + YEAR + ".txt", encoding="utf-8") as f:
+        puzzle_data = []
+        for line in f:
+            puzzle_data.append(line.strip())
+
+if DEBUG:
+    print(len(puzzle_data), puzzle_data)
+
+total_score = 0
+
+# "A X" = 4
+# "A Y" = 8
+# "A Z" = 3
+
+# "B X" = 1
+# "B Y" = 5
+# "B Z" = 9
+
+# "C X" = 7
+# "C Y" = 2
+# "C Z" = 6
+
+for game in puzzle_data:
+    
+    if game == "A X":
+        total_score += 4
+    
+    if game == "A Y":
+        total_score += 8
+
+    if game == "A Z":
+        total_score += 3
+
+    if game == "B X":
+        total_score += 1
+    
+    if game == "B Y":
+        total_score += 5
+
+    if game == "B Z":
+        total_score += 9
+
+    if game == "C X":
+        total_score += 7
+    
+    if game == "C Y":
+        total_score += 2
+
+    if game == "C Z":
+        total_score += 6
+
+ANSWER = total_score
+
+print(
+    "AoC Day: " + DAY + " Year: " + YEAR +
+    " part " + PART + ", answer:", ANSWER
+)
